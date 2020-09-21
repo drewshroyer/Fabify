59e53761dbf7f31393a60ebfd8c24c9c6c92b8f4
drewshroyer <drewshroyer@gmail.com>
2020-09-21 17:53:52 -0400

    Prepare for initial Heroku push

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     context: __dirname,
-    entry: './frontend/todo_redux.jsx',
+    entry: './frontend/index.jsx',
     output: {
         path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
         filename: 'bundle.js'
