aebe4244036ca3a4b1b7aa0f79aa46022de3e7e2
drewshroyer <drewshroyer@gmail.com>
2020-09-22 10:18:32 -0400

    beginning of frontend auth

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,8 +1,11 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
+// import configureStore from "./store/store";
 
+debugger
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+    debugger
+    // const store = configureStore();
+    ReactDOM.render(<h1>Welcome to Fabify</h1>, root);
 });
