bdc1b33de11001df3068cc3fd1dded0c1c80a941
drewshroyer <drewshroyer@gmail.com>
2021-01-07 16:34:42 -0500

    updated comments

--- frontend/components/modal.jsx
+++ frontend/components/modal.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { closeModal } from "../actions/modal_actions";
 import { connect } from "react-redux";
 import CreatePlaylistContainer from "./playlists/create_playlist_container";
-// import EditPlaylistContainer from "./playlists/playlist_edit_container";
 
 function Modal({ modal, closeModal }) {
   if (!modal) {
